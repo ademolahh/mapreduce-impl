0a5fc34a6a41e2f80435af3aabe2f29252660af8
A <aademolaoladipo@gmail.com>
2026-07-24 18:20:39 +0100

    refactor: extract path constants and add phase logging to sequential runner

--- sequential/main.go
+++ sequential/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"sort"
 )
+
+const READ_FOLDER string = "words/text"
+const OUT_PATH = "words/result/result.txt"
 
 func main() {
 	build := buildFlag()
@@ -15,19 +19,22 @@ func main() {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
-	const FOLDER string = "words/text"
-	kvs, err := runMapPhase(mps, FOLDER)
+	log.Println("plugin loaded")
+
+	kvs, err := runMapPhase(mps, READ_FOLDER)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
+	log.Println("map phase done")
 
 	sort.Sort(kvs)
 
-	if err := runReducePhase(kvs, rdc, "words/result/result.txt"); err != nil {
+	if err := runReducePhase(kvs, rdc, OUT_PATH); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
+	log.Println("reduce phase done")
 }
 
 func buildFlag() string {
